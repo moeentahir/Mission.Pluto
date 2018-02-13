63a0def096c91f493ce101af93016de97af36e23
Moeen Tahir <moeentahir@hotmail.com>
2018-02-13 20:51:23 +0000

    Moved Rover class to a separate file and then moved to class library projects

--- Mission.Pluto/Rover.cs
+++ Mission.Pluto/Rover.cs
@@ -1,41 +1,6 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace Mission.Pluto.UnitTests
+﻿namespace Mission.Pluto.UnitTests
 {
-    [TestClass]
-    public class RoverTests
-    {
-        [TestMethod]
-        [DataRow('N','W')]
-        [DataRow('W', 'S')]
-        [DataRow('S', 'E')]
-        [DataRow('E', 'N')]
-        public void Rotating_Left_Should_Change_Direction_Anti_Clock_Wise(char startingFace, char expected)
-        {
-            var rover = new Rover(0, 0, startingFace);
-            rover.Rotate('L');
-            var actual = rover.Face;
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        [DataRow('N', 'E')]
-        [DataRow('E', 'S')]
-        [DataRow('S', 'W')]
-        [DataRow('W', 'N')]
-        public void Rotating_Right_Should_Change_Direction_Clock_Wise(char startingFace, char expected)
-        {
-            var rover = new Rover(0, 0, startingFace);
-            rover.Rotate('R');
-            var actual = rover.Face;
-
-            Assert.AreEqual(expected, actual);
-        }
-    }
-
-    internal class Rover
+    public class Rover
     {
         public Rover(int x, int y, char face)
         {
