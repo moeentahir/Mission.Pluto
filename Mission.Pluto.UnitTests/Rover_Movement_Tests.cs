9ffec270bf1791a8e900b9bd6ecc91b8ba528a33
Moeen Tahir <moeentahir@hotmail.com>
2018-02-13 21:19:43 +0000

    Added test case to move backwards when facing North

--- Mission.Pluto.UnitTests/Rover_Movement_Tests.cs
+++ Mission.Pluto.UnitTests/Rover_Movement_Tests.cs
@@ -20,5 +20,17 @@ namespace Mission.Pluto.UnitTests
             Assert.AreEqual(expectedY, rover.Y);
             Assert.AreEqual(startingFace, rover.Face);
         }
+
+        [TestMethod]
+        [DataRow('N', 0, -1)]
+        public void Move_Backwards_When_Facing_In_Different_Directions(char startingFace, int expectedX, int expectedY)
+        {
+            var rover = new Rover(0, 0, startingFace);
+            rover.Move('B');
+
+            Assert.AreEqual(expectedX, rover.X);
+            Assert.AreEqual(expectedY, rover.Y);
+            Assert.AreEqual(startingFace, rover.Face);
+        }
     }
 }
