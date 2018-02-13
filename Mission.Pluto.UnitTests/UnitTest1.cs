b85f3bedf3402e9e6249e8d62da2f522ae101759
Moeen Tahir <moeentahir@hotmail.com>
2018-02-13 20:17:41 +0000

    Added Rover class and a failing test to rotate Rover

---
+++ Mission.Pluto.UnitTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mission.Pluto.UnitTests
+{
+    [TestClass]
+    public class RoverTests
+    {
+        [TestMethod]
+        public void When_Facing_North_Rotate_Left_Should_Face_West()
+        {
+            var rover = new Rover(0, 0, 'N');
+            rover.Rotate('L');
+            var expected = 'W';
+            var actual = rover.Face;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    internal class Rover
+    {
+        public Rover(int x, int y, char face)
+        {
+            X = x;
+            Y = y;
+            Face = face;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Face { get; set; }
+
+        internal void Rotate(char v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
